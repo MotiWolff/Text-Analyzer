7f751247626bc40292604ee7daa0c00e151b9edb
MotiWolff <motiwolff@gmail.com>
2025-05-05 09:52:35 +0300

    Remove dj_database_url dependency and simplify database configuration

--- cyberLibrary/settings.py
+++ cyberLibrary/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
