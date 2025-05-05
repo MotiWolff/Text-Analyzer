c8ae97bff000f7e11705b2f853fc4283648899ad
MotiWolff <motiwolff@gmail.com>
2025-05-05 09:49:11 +0300

    Initial commit: Text Analyzer Django application

--- cyberLibrary/settings.py
+++ cyberLibrary/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'cyberLibrary.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
