d42e7ac2e7b8c1719eeac5a652a313000396ef29
MotiWolff <motiwolff@gmail.com>
2025-05-05 09:37:32 +0300

    Prepare for Render deployment

--- cyberLibrary/settings.py
+++ cyberLibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
