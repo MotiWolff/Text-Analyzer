5ba702647c390340604b2ee874316b436145dad5
MotiWolff <motiwolff@gmail.com>
2025-05-05 10:09:11 +0300

    Add build logging and fix database path

--- build.sh
+++ build.sh
@@ -2,13 +2,20 @@
 # exit on error
 set -o errexit
 
+echo "Starting build process..."
+
 # Install dependencies
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
 # Run migrations
-python manage.py makemigrations text_analyzer
-python manage.py migrate text_analyzer
-python manage.py migrate
+echo "Running migrations..."
+python manage.py makemigrations text_analyzer --verbosity 3
+python manage.py migrate text_analyzer --verbosity 3
+python manage.py migrate --verbosity 3
 
 # Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Build process completed!"
